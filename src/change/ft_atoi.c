e4fb575799afd1c9b0454bbaaa17ee6f1ea18d76
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-01 12:46:40 -0600

    add ft_atoi.c

---
+++ src/change/ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 12:38:59 by diegofranci       #+#    #+#             */
+/*   Updated: 2022/04/01 12:46:15 by diegofranci      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	result;
+	int	positive;
+
+	i = 0;
+	positive = 1;
+	result = 0;
+	while (str[i] != '\0' && str[i] == 32)
+	{
+		i++;
+	}
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		{
+			positive *= -1;
+		}
+		i++;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		result = (str[i] - 48) + (result * 10);
+		i++;
+	}
+	return (result * positive);
+}
