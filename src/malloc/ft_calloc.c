959b088714cf2f5ef5f80e5c94e551a84a90d879
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-01 13:07:34 -0600

    add malloc funcions

---
+++ src/malloc/ft_calloc.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 12:48:06 by diegofranci       #+#    #+#             */
+/*   Updated: 2022/04/01 13:00:11 by diegofranci      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+//#include <string.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char		*str;
+	size_t		i;
+
+	i = 0;
+	str = malloc(count * size);
+	if (str == NULL)
+		return (0);
+	while (i < count * size)
+	{
+		str[i] = 0;
+		i++;
+	}
+	return (str);
+}
