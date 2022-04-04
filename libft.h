c7c8ab412394556b87ae46683f7f707a6a3f28c5
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-04 10:22:02 -0500

    libft.h se agregaron los src/is

--- libft.h
+++ libft.h
@@ -6,19 +6,27 @@
 /*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 19:11:08 by diegofranci       #+#    #+#             */
-/*   Updated: 2022/04/03 19:57:22 by diegofranci      ###   ########.fr       */
+/*   Updated: 2022/04/04 10:18:06 by diegofranci      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
 # endif
+
 # include <strings.h>
 # include <stdbool.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-
-
+/* Checker */
+int	ft_isalpha(const int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(char c);
+int	ft_isprint(char c);
 
 #endif
