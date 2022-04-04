32e846d2c2beb921784c47aac463baa9e339aaf2
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-04 10:21:31 -0500

    ft_substr ready

--- src/malloc/ft_substr.c
+++ src/malloc/ft_substr.c
@@ -6,32 +6,32 @@
 /*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 13:12:55 by diegofranci       #+#    #+#             */
-/*   Updated: 2022/04/03 20:11:38 by diegofranci      ###   ########.fr       */
+/*   Updated: 2022/04/04 10:19:06 by diegofranci      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdlib.h>
 #include "libft.h"
-
-static char	*empty_string(void)
-{
-	char	*result;
-	result = malloc(1);
-	if (result)
-		result[0] = 0;
-	return (result);
-}
-
-static size_t	select_smallest(size_t num1, size_t num2)
-{
-	if (num1 > num2)
-		return (num2);
-	return (num1);
-}
-
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	i;
+	size_t	ii;
+	char	*str;
 
+	str = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	ii = 0;
+	while (s[i])
+	{
+		if (i >= start && ii < len)
+		{
+			str[ii] = s[i];
+			ii++;
+		}
+		i++;
+	}
+	str[ii] = 0;
+	return (str);
 }
