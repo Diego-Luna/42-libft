abeb5031be7217c79e929a8715c76c6c8ffcff6d
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-03 20:12:52 -0500

    add libft

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/03 19:11:08 by diegofranci       #+#    #+#             */
+/*   Updated: 2022/04/03 19:57:22 by diegofranci      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+# include <strings.h>
+# include <stdbool.h>
+
+
+
+
+#endif
