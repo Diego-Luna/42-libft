22023576868abf812ba3ae4e7a31781db28c2db3
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-06 14:49:59 -0500

    Bonus part 4/9

---
+++ src/ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 14:33:51 by diegofranci       #+#    #+#             */
+/*   Updated: 2022/04/06 14:49:12 by diegofranci      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	while (lst)
+	{
+		if (!lst->next)
+		{
+			return (lst);
+		}
+		lst = lst->next;
+	}
+}
