923df03194a0b94dee820a2004329b6f64ded245
Diego-Luna <diegofcolunalopez@gmail.com>
2022-04-08 13:19:10 -0500

    ajuste en ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: diegofranciscolunalopez <diegofrancisco    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 19:33:03 by diegofranci       #+#    #+#             */
-/*   Updated: 2022/04/06 12:19:51 by diegofranci      ###   ########.fr       */
+/*   Updated: 2022/04/08 13:17:57 by diegofranci      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	unsigned int		i;
 
 	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	while (s1[i] && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
 		i++;
 	}
-	return (0);
+	if (s2[i] == '\0' || i == n)
+		return (0);
+	else
+		return (-s2[i]);
 }
